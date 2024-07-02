013717a0f811de4dfdec2232738a0dcb4c9ba647
cole <colenote1@gmail.com>
2024-07-01 18:09:57 -0700

    fortnight

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ def main():
     Code Your Program here
     ########################################
     """
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    num3 = int(input("Enter the third number: "))
+    num4 = int(input("Enter the fourth number: "))
+    num5 = int(input("Enter the fifth number: "))
+    numbers = [num1,num2,num3,num4,num5]
+    for i  in range(0,5):
+        if numbers[i] >= numbers[0] and numbers[1] and numbers[2] and numbers[3] and numbers[4]:
+            maxval = numbers[i]
+        elif numbers[i] <= numbers[0] and numbers[1] and numbers[2] and numbers[3] and numbers[4]:
+            minval = numbers[i]
 
     print(*numbers)
     print(maxval, minval)
